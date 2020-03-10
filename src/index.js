4bc0c5eb9168fd496103d9309b240390ee39a2af
Natalia Ivanova <mumplini@gmail.com>
2020-03-10 20:55:29 +0100

    feat: provide implementation for JS: Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var pairToCloseBracket = Object.assign(...bracketsConfig.map(([open, close]) => ({[close]: open})));
+return str.split('').reduce(function(singleBrackets, bracket){
+  var len = singleBrackets.length;
+      if (len > 0 && singleBrackets[len-1] === pairToCloseBracket[bracket])
+        singleBrackets.pop(); 
+      else 
+        singleBrackets.push(bracket); 
+      return singleBrackets;
+    }, []).length == 0;
 }
