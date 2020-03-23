75d2ada32635032137d454424a40077499cbe7ce
Natalia Ivanova <mumplini@gmail.com>
2020-03-23 19:50:49 +0100

    fix: reformat code style and fix vars declaration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  var pairToCloseBracket = Object.assign(...bracketsConfig.map(([open, close]) => ({[close]: open})));
-return str.split('').reduce(function(singleBrackets, bracket){
-  var len = singleBrackets.length;
-      if (len > 0 && singleBrackets[len-1] === pairToCloseBracket[bracket])
-        singleBrackets.pop(); 
-      else 
-        singleBrackets.push(bracket); 
-      return singleBrackets;
-    }, []).length == 0;
+  const pairToCloseBracket = Object.assign(...bracketsConfig.map(
+    ([open, close]) => ({ [close]: open }),
+  ));
+  return str.split('').reduce((singleBrackets, bracket) => {
+    const len = singleBrackets.length;
+    if (len > 0 && singleBrackets[len - 1] === pairToCloseBracket[bracket]) singleBrackets.pop();
+    else singleBrackets.push(bracket);
+    return singleBrackets;
+  }, []).length === 0;
 }
